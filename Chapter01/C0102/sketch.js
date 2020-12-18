6a13650655fd812bba8aa8587e58da44036ce41b
Heo JongMyung <chaqwe270@gmail.com>
2020-12-18 20:11:31 +0900

    Add Particle of Color is random

--- Chapter01/C0102/sketch.js
+++ Chapter01/C0102/sketch.js
@@ -1,85 +1,61 @@
-/// <reference path= "../../lib/p5.global-mode.d.ts"/>
-
-       let g_MassArray = [];
-       let g_PositionXArray = [];
-       let g_PositionYArray = [];
-       let g_VelocityXArray = [];
-       let g_VelocityXArray = [];
+let g_MassArray = [];
+let g_positionXArray = [];
+let g_positionYArray = [];
+let g_velocityXArrat = [];
+let g_velocityYArrat = [];
 
 
-        function setup()
-        {
-            createCanvas( 800, 600 );
-            noStroke();
-            fill(64, 255, 255, 150);
-        }
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	noStroke();
+	fill(64, 255, 255, 192);
+}
 
-        function draw()
-        {
-            background( 96, 96, 96 );
+function draw() {
+	background(32);
+	
+	for (let i = 0; i < g_MassArray.length; i++) {
+		let accelerationX = 0, accelerationY = 0;
+		
+		for (let j = 0; j < g_MassArray.length; j++) {
+			if (i != j) {
+				let distanceX = g_positionXArray[j] - g_positionXArray[i];
+				let distanceY = g_positionYArray[j] - g_positionYArray[i];
 
-            for (let particleA = 0; particleA < g_MassArray.length; particleA++) {
-                let accelerationX = 0, accelerationY = 0;
-                
-                for (let particleB = 0; particleB < g_MassArray.length; particleB++) {
-                    if (particleA != particleB) {
-                        let distanceX = g_PositionXArray[particleB] - g_PositionXArray[particleA];
-                        let distanceY = positionY[particleB] - positionY[particleA];
+				let distance = sqrt(distanceX * distanceX + distanceY * distanceY);
+				if (distance < 1) distance = 1;
+
+				let force = (distance - 320) * g_MassArray[j] / distance;
+				accelerationX += force * distanceX;
+				accelerationY += force * distanceY;
+			}
+		}
+		
+		g_velocityXArrat[i] = g_velocityXArrat[i] * 0.99 + accelerationX * g_MassArray[i];
+		g_velocityYArrat[i] = g_velocityYArrat[i] * 0.99 + accelerationY * g_MassArray[i];
+	}
+	
+	for (let particle = 0; particle < g_MassArray.length; particle++) {
+		g_positionXArray[particle] += g_velocityXArrat[particle];
+		g_positionYArray[particle] += g_velocityYArrat[particle];
         
-                        let distance = sqrt(distanceX * distanceX + distanceY * distanceY);
-                        if (distance < 1) distance = 1;
-        
-                        let force = (distance - 320) * g_MassArray[particleB] / distance;
-                        accelerationX += force * distanceX;
-                        accelerationY += force * distanceY;
-                    }
-                }
-                
-                g_VelocityXArray[particleA] = g_VelocityXArray[particleA] * 0.99 + accelerationX * g_MassArray[particleA];
-                g_VelocityXArray[particleA] = g_VelocityXArray[particleA] * 0.99 + accelerationY * g_MassArray[particleA];
-            }
-            
-            for (let particle = 0; particle < g_MassArray.length; particle++) {
-                g_PositionXArray[particle] += g_VelocityXArray[particle];
-                positionY[particle] += g_VelocityXArray[particle];
-                
-                ellipse(g_PositionXArray[particle], positionY[particle], g_MassArray[particle] * 1000, g_MassArray[particle] * 1000);
-            }
-        }
-        
+        fill(random(0,255), random(0,255), random(0,255));
+		ellipse(g_positionXArray[particle], g_positionYArray[particle], g_MassArray[particle] * 1000, g_MassArray[particle] * 1000);
+	}
+}
 
-        function addNewParticle(){
-            g_MassArray.push(random(0.003, 0.03));
-            g_PositionXArray.push(mouseX);
-            positionY.push(mouseY);
-            g_VelocityXArray.push(0);
-            g_VelocityXArray.push(0);
-        }
+function addNewParticle( positionX, positionY) {
+	g_MassArray.push(random(0.003, 0.03));
+	g_positionXArray.push(positionX);
+	g_positionYArray.push(positionY);
+	g_velocityXArrat.push(0);
+	g_velocityYArrat.push(0);
+}
 
-        //bug
-        function minusNewParticle(){
-            g_MassArray.remove(random(0.003, 0.03));
-            g_PositionXArray.remove(mouseX);
-            positionY.remove(mouseY);
-            g_VelocityXArray.remove(0);
-            g_VelocityXArray.remove(0);
-        }
-        
+function mouseClicked() {
+	addNewParticle(mouseX, mouseY);
+}
 
-        function mouseClicked() {
-            if(mousePressed == LEFT)
-            {
-                addNewParticle();
-            }
-
-
-            //bug 
-            if(mousePressed == RIGHT)
-            {
-                minusNewParticle();
-            }
-        }
-
-        function mouseDragged() {
-            addNewParticle();
-        }
+function mouseDragged() {
+	addNewParticle(mouseX, mouseY);
+}
